401b596df104df10d02ece72995532cc775ee898
Hari <kathulaharish929311@gmail.com>
2023-11-20 20:27:48 +0530

    add close button activated

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import { MdOutlineClose } from "react-icons/md";
+import { useState } from 'react';
 
 function App() {
 
+  const [addSection, setAddSection] = useState(false)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
   return (
     <>
       <div className="container">
-        <button className="btn btn-add">Add</button>
-        <div className='addContainer'>
+        <button className="btn btn-add" onClick={()=>setAddSection(true)}>Add</button>
+
+        {
+          addSection && (
+            <div className='addContainer'>
           
-          <form>
-            <div className="close-btn"><MdOutlineClose/></div>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name"/>
+            <form onSubmit={handleSubmit}>
+              <div className="close-btn" onClick={()=>setAddSection(false)}><MdOutlineClose/></div>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name"/>
+  
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email"/>
+  
+              <label htmlFor="mobile">Mobile</label>
+              <input type="number" id="mobile" name="mobile"/>
+  
+              <button className="btn">Submit</button>
+            </form>
+          </div>
+          )
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email"/>
+        }
 
-            <label htmlFor="mobile">Mobile</label>
-            <input type="number" id="mobile" name="mobile"/>
-
-            <button className="btn">Submit</button>
-          </form>
-        </div>
 
       </div>
     </>
